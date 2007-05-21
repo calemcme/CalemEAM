e2dd0bb21e636d1f38a2b8c8a22f10c51ed5ea30
CalemEAM <calemcme@users.noreply.github.com>
2007-05-21 23:36:28 +0000

    added sample config for linux install

--- server/conf/custom.sample.php
+++ server/conf/custom.sample.php
@@ -5,6 +5,6 @@
 $_CALEM_dist['calem_root_uri']='/CalemEAM';
 $_CALEM_dist['calem_request_uri']='/CalemEAM/index.php';
 $_CALEM_dist['calem_soap_uri']='/CalemEAM/CalemSoapService.php';
-$_CALEM_dist['jsminClass']=array('path'=>'build', 'class'=>'CalemJsMinPhp');
+$_CALEM_dist['jsminClass']=array('path'=>'build', 'class'=>'CalemJsMinExe');
 $_CALEM_dist['jsmin_exe']='build/jsmin/jsmin.exe';
 ?>
