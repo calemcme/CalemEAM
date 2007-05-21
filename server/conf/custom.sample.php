87e2627a8b29043b3b68d41270aeee2d03220d80
CalemEAM <calemcme@users.noreply.github.com>
2007-05-21 21:20:27 +0000

    added linux x86 jsmin

--- server/conf/custom.sample.php
+++ server/conf/custom.sample.php
@@ -6,4 +6,5 @@ $_CALEM_dist['calem_root_uri']='/CalemEAM';
 $_CALEM_dist['calem_request_uri']='/CalemEAM/index.php';
 $_CALEM_dist['calem_soap_uri']='/CalemEAM/CalemSoapService.php';
 $_CALEM_dist['jsminClass']=array('path'=>'build', 'class'=>'CalemJsMinPhp');
+$_CALEM_dist['jsmin_exe']='build/jsmin/jsmin.exe';
 ?>
