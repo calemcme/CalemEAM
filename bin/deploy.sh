de4fe57360030802a196f4086273eaf924cd19a8
CalemEAM <calemcme@users.noreply.github.com>
2007-06-06 21:28:35 +0000

    m12 - jsmin issue

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 . env.sh
+CALEM_BUILD=0
+export CALEM_BUILD
+# process inputs
+for i in $*
+	do
+	case "$1" in
+		build)
+	        CALEM_BUILD=1
+			export CALEM_BUILD
+	      ;;
+	esac
+	done	  
 $PHP_EXE $CALEM_HOME/build/CalemZipJsCmd.php
