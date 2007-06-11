0e7071ca96c836b22670a1be0bae6e8c441451a0
clayli <clayli@d911e636-ecbe-fb47-a972-939408e71800>
2007-06-11 00:52:47 +0000

    r1.0.2rc1 merged back to trunk.

--- trunk/bin/env.sample.sh
+++ trunk/bin/env.sample.sh
@@ -2,3 +2,4 @@ HOME_DIR=/opt/lampp
 PHP_EXE=$HOME_DIR/bin/php
 CALEM_HOME=$HOME_DIR/htdocs/CalemEAM
 export CALEM_DIR=$CALEM_HOME/
+
--- trunk/server/conf/calem.custom.sample.php
+++ trunk/server/conf/calem.custom.sample.php
@@ -9,8 +9,18 @@ $_CALEM_dist['calem_db_password'] = 'calemeam';
 
 //Application host
 $_CALEM_dist['calem_application_host'] = 'localhost';
+$_CALEM_dist['calem_application_port'] = '';
 
 //Admin user name and password to create Calem DB
 $_CALEM_dist['db_admin_user'] = 'root';
 $_CALEM_dist['db_admin_password'] = '';
+
+/**
+* URI configuration
+*/
+$_CALEM_dist['calem_root_uri']='/CalemEAM';
+$_CALEM_dist['calem_request_uri']='/CalemEAM/index.php';
+$_CALEM_dist['calem_soap_uri']='/CalemEAM/CalemSoapService.php';
+$_CALEM_dist['jsminClass']=array('path'=>'build', 'class'=>'CalemJsMinExe');
+$_CALEM_dist['jsmin_exe']='build/jsmin/jsmin.exe';
 ?>
