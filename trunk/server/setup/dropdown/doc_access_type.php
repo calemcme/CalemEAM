506cd9f2458c3b51cedbfc77eabba8ff0f7dd2ee
clayli <clayli@d911e636-ecbe-fb47-a972-939408e71800>
2007-09-30 06:49:13 +0000

    m-28 doc upload

---
+++ trunk/server/setup/dropdown/doc_access_type.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  Copyright (c) 2006-2007  
+ *  CalemEAM Inc. All Rights Reserved.
+ *  The contents of this file are confidential and proprietary information of CalemEAM Inc. 
+ *  You shall use the file only in accordance with the terms of the CalemEAM Software License Agreement.
+ */
+
+//Checking basic initialization
+if (!defined('_CALEM_DIR_')) die("Access denied at ".__FILE__);
+
+$data=array(
+	'doc_access_public'=>array('fixed'=>1),
+	'doc_access_app'=>array('fixed'=>1)
+);
+?>
