cc78e4a546c8fcae7cb1cec1c87b0098c7e5d565
clayli <clayli@d911e636-ecbe-fb47-a972-939408e71800>
2007-10-30 19:58:01 +0000

    m-60 add comments to env.bat/sh

--- trunk/bin/env.sample.bat
+++ trunk/bin/env.sample.bat
@@ -1,5 +1,10 @@
 echo off
+REM HOME_DIR is the root location of your website. 
+REM Default WAMP root directory is c:\wamp
 set HOME_DIR=c:\wamp
+REM This is the location of your php exe file
 set PHP_EXE=%HOME_DIR%\php\php.exe
+REM This is the location of your CalemEAM root directory
+REM Export CALEM_DIR which is used in PHP scripts
 set CALEM_DIR=%HOME_DIR%\www\CalemEAM\
 
