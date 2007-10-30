767264addf614f9458d2fd9090bcaba32f0fce86
CalemEAM <calemcme@users.noreply.github.com>
2007-10-30 19:58:01 +0000

    m-60 add comments to env.bat/sh

--- bin/env.sample.sh
+++ bin/env.sample.sh
@@ -1,5 +1,10 @@
+# HOME_DIR is the root location of your website. 
+# Default XAMPP home directory is /opt/lampp if you have root access, or /home/<username>/www on a production server 
 HOME_DIR=/opt/lampp
+# This is the location of your php exe file
 PHP_EXE=$HOME_DIR/bin/php
+# This is the location of your CalemEAM root directory and could also be /$HOME_DIR/CaleamEAM
 CALEM_HOME=$HOME_DIR/htdocs/CalemEAM
+# Export CALEM_DIR which is used in PHP scripts
 export CALEM_DIR=$CALEM_HOME/
 
