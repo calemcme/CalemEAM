de91b759cfb1ef7b45ebd6a81c2bcf38643229fe
CalemEAM <calemcme@users.noreply.github.com>
2008-03-17 21:34:15 +0000

    m-111 set path to scheduler script's path.

--- bin/scheduler.bat
+++ bin/scheduler.bat
@@ -1,5 +1,5 @@
-REM To run this script with a scheduler uncomment next line and set the path to the bin directory of your installation
-REM cd \wamp\www\CalemEAM\bin
+REM Reset working directory to current directory
+cd /d %~dp0
 call env.bat
 
 %PHP_EXE% scheduler\CalemSchedulerCmd.php
--- bin/scheduler.sh
+++ bin/scheduler.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# To run this script with a scheduler uncomment next line and set the path to the bin directory of your installation
-# cd /opt/lampp/htdocs/CalemEAM/bin
+# reset directory to current script directory
+cd `dirname $0`
 . env.sh  
 $PHP_EXE scheduler/CalemSchedulerCmd.php
