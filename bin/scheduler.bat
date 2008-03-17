67efcb644305da989ccf793388ce05fb1e10c6ed
CalemEAM <calemcme@users.noreply.github.com>
2008-03-17 19:57:03 +0000

    Added comments for running the scripts with schedulers.

--- bin/scheduler.bat
+++ bin/scheduler.bat
@@ -1,3 +1,5 @@
+REM To run this script with a scheduler uncomment next line and set the path to the bin directory of your installation
+REM cd \wamp\www\CalemEAM\bin
 call env.bat
 
 %PHP_EXE% scheduler\CalemSchedulerCmd.php
--- bin/scheduler.sh
+++ bin/scheduler.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+# To run this script with a scheduler uncomment next line and set the path to the bin directory of your installation
+# cd /opt/lampp/htdocs/CalemEAM/bin
 . env.sh  
 $PHP_EXE scheduler/CalemSchedulerCmd.php
