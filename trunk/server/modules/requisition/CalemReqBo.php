077b4e89d547adf1d63e52f8adb45d283f179b7e
clayli <clayli@d911e636-ecbe-fb47-a972-939408e71800>
2008-04-07 18:30:42 +0000

    m-116

--- trunk/server/modules/requisition/CalemReqBo.php
+++ trunk/server/modules/requisition/CalemReqBo.php
@@ -238,3 +238,5 @@ class CalemReqBo extends CalemBo {
 		$this->onReqItemChanged($inId, $reqId, $qty, $uc, $uom);			      
 	} 
 }
+
+?>
--- trunk/server/modules/requisition/CalemReqDbo.php
+++ trunk/server/modules/requisition/CalemReqDbo.php
@@ -94,3 +94,5 @@ class CalemReqDbo extends CalemDbo {
 		}
 	} 
 }
+
+?>
