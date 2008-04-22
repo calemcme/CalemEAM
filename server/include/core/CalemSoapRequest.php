39f72c6306d7573449b503c359888ed70496d621
CalemEAM <calemcme@users.noreply.github.com>
2008-04-22 02:29:41 +0000

    Refactor soap service to rollback to r2.0.1 edition.

--- server/include/core/CalemSoapRequest.php
+++ server/include/core/CalemSoapRequest.php
@@ -17,35 +17,43 @@
  
  * Contributor(s): 
  */
+
  
 //Checking basic initialization
 if (!defined('_CALEM_DIR_')) die("Access denied at ".__FILE__);
 
+require_once _CALEM_DIR_ . 'server/conf/soap_map.php';
+require_once _CALEM_DIR_ . 'server/include/log4php/LoggerManager.php';
+require_once _CALEM_DIR_ . 'server/include/util/CalemExit.php';
+require_once _CALEM_DIR_ . 'server/include/core/session/CalemSession.php';
 require_once _CALEM_DIR_ . 'server/include/util/CalemGzip.php';
-require_once _CALEM_DIR_ . 'server/include/core/CalemWsInterface.php';
 
 require_once _CALEM_DIR_ . 'server/include/SOAP/Base.php';
 require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
 
 /**
- * This is the SOAP Service Handler
+ * This is the CalemSoapRequest handler base
  */
- class CalemWsSoap implements CalemWsInterface{
+ class CalemSoapRequest {
+ 	/**
+         * @todo there're issues related to XML format.
+         * It appears to do with the way the XML doc is morphed into JSON.
+         */
  	protected $responseFormat;
  	protected $logger;
  	protected $reqHeaders;
  	protected $method;
  	protected $methodResponse;
  	protected $params;
+ 	//Configuration
+ 	protected $contentType;
  	//Soap object for parsing
  	protected $soapBase;
  	//xml serializer
  	protected $sz;
- 	//Service object
- 	protected $so;
  	
  	public function __construct() {
- 		$this->logger=&LoggerManager::getLogger('CalemWsSoap');
+ 		$this->logger=&LoggerManager::getLogger('CalemSoapRequest');
  		if ($this->logger->isDebugEnabled()) { //For debug test purpose only.
 	 		$this->method="unknown";
 	 		$this->methodResponse="unknownResponse";
@@ -59,24 +67,43 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	}
  	
  	/**
- 	 * Set the values for this handler.
+ 	 * This is the main soap service entry
  	 * @param Array SOAP request headers
  	 * @param String SOAP request method
  	 * @param Array SOAP request parameters
  	 * @return mixed results
  	 */
- 	public function init($reqHeaders, $method, $params, $so) {
- 		$this->reqHeaders=$reqHeaders;
- 		$this->method=$method;
- 		$this->methodResponse = $method . 'Response';
- 		$this->params=$params;
- 		$this->so=$so;
- 		//Set up response format now
- 		$this->setResponseFormat($this->reqHeaders);	 		 		
+ 	public function service($reqHeaders, $method, $params) {
+ 		try {
+	 		$this->reqHeaders=$reqHeaders;
+	 		$this->method=$method;
+	 		$this->methodResponse = $method . 'Response';
+	 		$this->params=$params;
+	 		if ($this->authenticationRequired() && !$this->authenticate()) {
+	 			$this->sendFault(CALEM_SF_NO_SESSION, 'No active session'); //Exit from here
+	 		}
+	 		//Set up response format now
+	 		$this->setResponseFormat($this->reqHeaders);
+	 		//Let's dispatch the call now.
+	 		$response=call_user_func(array($this, $this->method));
+	 		if (!$response || !is_array($response)) {
+	 			$this->sendFault(CALEM_SF_UNKNOWN);
+	 		}
+	 		//Encode the result properly
+	 		$env=call_user_func(array($this, 'encodeResponse_'.$this->responseFormat), $response);
+	 		global $_CALEM_conf;
+	 		$this->sendResponse($_CALEM_conf['calem_soap_status'], $env, $this->gzipIt());
+ 		} catch (CalemSoapException $ce) {
+ 			$this->logger->error("Error in processing soap request: " . $e->getMessage());
+ 			$this->sendFault($ce->getCode(), $e->getMessage());
+ 		} catch (Exception $e) {
+ 			$this->logger->error("Error in processing soap request: " . $e->getMessage());
+ 			$this->sendFault(CALEM_SF_UNKNOWN, $e->getMessage());
+ 		} 
  	}
  	
  	/**
- 	 * Interface service - provide parameters.
+ 	 * fetch params
  	 */
  	public function getParamValue($n=0) {
  		if ( !isset($this->params) || $n > count($this->params) ) return null;
@@ -84,39 +111,9 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	}
  	
  	/**
- 	 * Interface service - provide SID from client
- 	 */
- 	public function getSid() {
- 		$sid=null;
- 		if (!$this->reqHeaders || !is_array($this->reqHeaders)) return $sid;
- 		//Let's find out the sessionId field
- 		foreach ($this->reqHeaders as $reqh) {
- 			if ($reqh->name === CALEM_SOAP_SESSIONID) {
- 				$sid=$reqh->value;
- 				break;
- 			}
- 		}
- 		return $sid;
- 	}
- 	
- 	/**
- 	 * Interface - send result back to client.
- 	 */
- 	public function sendResult($response) {
- 	   $env=call_user_func(array($this, 'encodeResponse_'.$this->responseFormat), $response);
-	 	global $_CALEM_conf;
-	 	$this->sendResponse($_CALEM_conf['calem_soap_status'], $env, $this->gzipIt());	
- 	}
- 	
- 	//Service object callback
- 	private function gzipIt() {
- 		return $this->so->gzipIt();
- 	}
- 	
- 	/**
  	 * Serialize using Pear's XML serializer
  	 */
-	private function serializeXml($data, $rootTag, $rootAttr=null) {
+	public function serializeXml($data, $rootTag, $rootAttr=null) {
 		//Reset given options
 		$this->sz->setOption('rootName', $rootTag);
 		if (isset($rootAttr)) $this->sz->setOption('rootAttributes', $rootAttr);
@@ -136,7 +133,7 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	 * @param Array Response
  	 * Use PEAR's XML Serializer to generate the xml from php response.
  	 */
- 	private function encodeResponse_XML($response) {
+ 	public function encodeResponse_XML($response) {
  		$xml= $this->serializeXml($response, "ns1:". $this->methodResponse);
  		return $this->packageSoapResponse($xml);
  	}
@@ -149,14 +146,14 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	 * The conversion is done using json_encode utility by enabling json extesion 
  	 * in php.ini.
  	 */
- 	private function encodeResponse_JS($response) {
+ 	public function encodeResponse_JS($response) {
  		return '{Body:{'. $this->methodResponse. ':'. json_encode($response) . '}}'; 		
  	}
  	
  	/**
  	 * Providing common soap namespaces
  	 */
- 	private function getSoapNamespaces() {
+ 	public static function getSoapNamespaces() {
  		return "<env:Envelope xmlns:env=\"http://www.w3.org/2003/05/soap-envelope\""
  		       ." xmlns:ns1=\"CalemSoapService\""
  		       ." xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
@@ -171,7 +168,7 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	 */
  	private function packageSoapResponse($xmlResponse) {
  		global $_CALEM_conf;
- 		$env= $_CALEM_conf['calem_soap_xmlheader'] . $this->getSoapNamespaces(); 
+ 		$env= $_CALEM_conf['calem_soap_xmlheader'] . self::getSoapNamespaces(); 
  		$env .= "<env:Body xmlns:rpc=\"http://www.w3.org/2003/05/soap-rpc\">";
  		$env .= $xmlResponse;
    	$env .="</env:Body>";
@@ -185,7 +182,7 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	public function sendFault($faultcode, $faultstring='', $faultdetail='')  {
  		$this->logger->error("Soap fault for method: " . $this->method . " is: " . $faultcode);
  		global $_CALEM_conf;
- 		$env= $_CALEM_conf['calem_soap_fault_xmlheader'] . $this->getSoapNamespaces();
+ 		$env= $_CALEM_conf['calem_soap_fault_xmlheader'] . self::getSoapNamespaces();
  		$env .="<env:Body><env:Fault>";
 		$env .="<faultcode xsi:type=\"xsd:string\">" . $faultcode . "</faultcode>";
 		$env .="<faultactor xsi:type=\"xsd:string\">" . 'CalemSoapServer' . "</faultactor>";
@@ -196,9 +193,24 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  	}
  	
  	/**
+ 	 * By default all requests require authentication
+ 	 * @return boolean true
+ 	 */
+ 	public function authenticationRequired() {
+ 		return true;
+ 	}
+ 	
+ 	/**
+ 	 * Request to zip the content - false by default.
+ 	 */
+ 	public function gzipIt() {
+ 		return false;
+ 	}
+ 	
+ 	/**
  	 * Response format parsing - either js or xml
  	 */
- 	private function setResponseFormat($reqhs) {
+ 	protected function setResponseFormat($reqhs) {
  		if (!$reqhs) return;
  		if (is_array($reqhs)) {
  			foreach ($reqhs as $reqh) {
@@ -210,6 +222,32 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
  			$this->responseFormat=strtoupper($reqhs->value);
  		}
 	}
+ 	
+ 	/**
+ 	 * Authenticate the request by inspecting the sessionId passed in.
+ 	 */
+ 	private function authenticate() {
+ 		if (!$this->reqHeaders || !is_array($this->reqHeaders)) return false;
+ 		//Let's find out the sessionId field
+ 		foreach ($this->reqHeaders as $reqh) {
+ 			if ($reqh->name === CALEM_SOAP_SESSIONID) {
+ 				$sid=$reqh->value;
+ 				break;
+ 			}
+ 		}
+ 		if (!isset($sid)) {
+ 			$this->logger->error("sid not set, authentication failed");
+ 			return false;
+ 		}
+ 		//Verify sessionId and lifetime
+ 		$sesReload=CalemSession::load($sid);
+ 		if (!isset($sesReload)) return false;
+ 		//Refresh timeout setting.
+ 		$sesReload->renew();
+ 		//Keep track of the userId for data operation
+ 		$GLOBALS['calem_ses_data']=$sesReload->get('user');			
+ 		return true;
+ 	}
  	
  	/**
  	 * Send a response back to client
@@ -227,6 +265,17 @@ require_once _CALEM_DIR_ . 'server/include/XML/Serializer.php';
 		CalemGzip::gzStart();
 		print $response;
 		CalemGzip::gzSoapCompressEndFlush($status, $zip, $encoding, $this->logger);
+ 	}
+ 	
+ 	/**
+ 	 * Shutdown the script environment
+ 	 * <ul>
+ 	 * <li> shutdown the logger
+ 	 * <li> exit the script
+ 	 * </ul>
+ 	 */
+ 	private function shutdown() {
+ 		CalemExit::exitCalem();
  	}
  }
 ?>
