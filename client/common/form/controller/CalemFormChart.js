4cc0c92fa7e56ac7ea03692c11853b561787fcf4
Alice <goldfeyesh@users.noreply.github.com>
2016-06-02 23:16:01 +0000

    change flash am charts with c3 for the daily wo count

--- client/common/form/controller/CalemFormChart.js
+++ client/common/form/controller/CalemFormChart.js
@@ -13,19 +13,19 @@
  * The Initial Developer of the Original Code is CalemEAM Inc.
  * Portions created by CalemEAM are Copyright (C) 2007 CalemEAM Inc.;
  * All Rights Reserved.
- 
- * Contributor(s): 
+
+ * Contributor(s):
  */
 
 /**
  * CalemFormChart
  * This is the read only view.
- * 
+ *
  */
 function CalemFormChart(parent, formId, data) {
 	if (arguments.length==0) return;
 	CalemForm.call(this, parent, formId, data);
-	this._printListener=new AjxListener(this, this.onPrint);	
+	this._printListener=new AjxListener(this, this.onPrint);
 	this._reload=['&',CalemConf['chart_engine']['forceload'],'=1'].join('');
 	this._resetData=new AjxTimedAction(this, this._onResetData);
 }
@@ -34,7 +34,7 @@ CalemFormChart.prototype = new CalemForm;
 CalemFormChart.prototype.constructor = CalemFormChart;
 
 CalemFormChart.prototype.toString = function() { return "CalemFormChart"; }
-	 
+
 /**
  * Load data for read form.
  */
@@ -46,15 +46,17 @@ function() {
 //To have chart component to reload.
 CalemFormChart.prototype._onDataRefresh =
 function() {
-	this._chartObj.reloadData(this._dataParam+this._reload);
-	//Set a timer to reset the info not to force refresh.
-	AjxTimedAction.scheduleAction(this._resetData, CalemConf['chart_engine']['resetDelay']);
+   this._chartObj.src = this._dataParam+this._reload;
+ //this._chartObj.reloadData(this._dataParam+this._reload);
+ //Set a timer to reset the info not to force refresh.
+ //AjxTimedAction.scheduleAction(this._resetData, CalemConf['chart_engine']['resetDelay']);
 }
 
 //Reset data load without reload
 CalemFormChart.prototype._onResetData =
 function() {
-	this._chartObj.reloadData(this._dataParam);
+   this._chartObj.src = this._dataParam;
+ //this._chartObj.reloadData(this._dataParam);
 }
 
 /**
@@ -77,7 +79,7 @@ function(info) {
 	var render;
 	if (!(render=CalemConf['view_engine']['viewReadRender'][info.getClassName()])) {
 		render=CalemForm.prototype.getRender.call(this, info);
-	} 
+	}
 	return render;
 }
 
@@ -87,7 +89,7 @@ function(normType) {
 	if (!(render=CalemConf['view_engine']['viewReadRender']['FieldRenders'][normType])
 	  &&!(render=CalemConf['view_engine']['viewReadRender']['FieldRenders']['default'])) {
 		render=CalemForm.prototype.getFieldRender.call(this, normType);
-	} 
+	}
 	return render;
 }
 
@@ -101,7 +103,7 @@ function(dataParam) {
 	this._dataParam=dataParam;
 }
 
-//Business logic for form read 
+//Business logic for form read
 
 //Get form design
 CalemFormChart.prototype.getDesign =
